a5b3b37ed6acd02e31f62ebe8e0f6083dae25db5
K4rl1Hub <michael@dr-karlstetter.de>
2025-12-18 21:35:56 +0100

    change sensor to async_setup

--- custom_components/warema_webcontrol/sensor.py
+++ custom_components/warema_webcontrol/sensor.py
@@ -19,7 +19,10 @@ class WebControlLanguageSensor(SensorEntity):
         self._client = client
         self._attr_name = "WebControl Sprache"
         self._attr_unique_id = "webcontrol_language"
-        self._attr_device_info = DeviceInfo(identifiers={(DOMAIN, "webcontrol")}, name="Warema WebControl")
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, "webcontrol")},
+            name="Warema WebControl",
+        )
 
     @property
     def native_value(self):
@@ -28,9 +31,12 @@ class WebControlLanguageSensor(SensorEntity):
 
     @property
     def extra_state_attributes(self):
-        return {"code": self._client.language, "base_url": self._client.base_url}
+        return {
+            "code": self._client.language,
+            "base_url": self._client.base_url
+        }
 
 
-def setup_platform(hass: HomeAssistant, config, add_entities, discovery_info=None):
+async def async_setup_entry(hass, entry, async_add_entities):
     client = hass.data[DOMAIN]["client"]
-    add_entities([WebControlLanguageSensor(client)], True)
+    async_add_entities([WebControlLanguageSensor(client)], True)
